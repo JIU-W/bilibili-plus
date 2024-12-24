6ce551675355d81e27040e99a631ace551cc8794
JIU-W <2661727280@qq.com>
2024-12-24 17:35:14 +0800

    关注、粉丝部分

---
+++ bilibili-plus-common/src/main/java/com/itjn/entity/dto/CountInfoDto.java
@@ -0,0 +1,23 @@
+package com.itjn.entity.dto;
+
+
+public class CountInfoDto {
+    private Integer playCount;
+    private Integer likeCount;
+
+    public Integer getPlayCount() {
+        return playCount;
+    }
+
+    public void setPlayCount(Integer playCount) {
+        this.playCount = playCount;
+    }
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+}
