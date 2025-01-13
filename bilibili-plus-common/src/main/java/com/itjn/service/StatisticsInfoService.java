6678b587e07e1fe1e4e3655efc62ebf49c41d665
JIU-W <2661727280@qq.com>
2025-01-13 16:44:35 +0800

    创作中心数据统计

---
+++ bilibili-plus-common/src/main/java/com/itjn/service/StatisticsInfoService.java
@@ -0,0 +1,90 @@
+package com.itjn.service;
+
+import com.itjn.entity.po.StatisticsInfo;
+import com.itjn.entity.query.StatisticsInfoQuery;
+import com.itjn.entity.vo.PaginationResultVO;
+
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * 数据统计 业务接口
+ */
+public interface StatisticsInfoService {
+
+    /**
+     * 根据条件查询列表
+     */
+    List<StatisticsInfo> findListByParam(StatisticsInfoQuery param);
+
+    /**
+     * 根据条件查询列表
+     */
+    Integer findCountByParam(StatisticsInfoQuery param);
+
+    /**
+     * 分页查询
+     */
+    PaginationResultVO<StatisticsInfo> findListByPage(StatisticsInfoQuery param);
+
+    /**
+     * 新增
+     */
+    Integer add(StatisticsInfo bean);
+
+    /**
+     * 批量新增
+     */
+    Integer addBatch(List<StatisticsInfo> listBean);
+
+    /**
+     * 批量新增/修改
+     */
+    Integer addOrUpdateBatch(List<StatisticsInfo> listBean);
+
+    /**
+     * 多条件更新
+     */
+    Integer updateByParam(StatisticsInfo bean, StatisticsInfoQuery param);
+
+    /**
+     * 多条件删除
+     */
+    Integer deleteByParam(StatisticsInfoQuery param);
+
+    /**
+     * 根据StatisticsDateAndUserIdAndDataType查询对象
+     */
+    StatisticsInfo getStatisticsInfoByStatisticsDateAndUserIdAndDataType(String statisticsDate, String userId, Integer dataType);
+
+
+    /**
+     * 根据StatisticsDateAndUserIdAndDataType修改
+     */
+    Integer updateStatisticsInfoByStatisticsDateAndUserIdAndDataType(StatisticsInfo bean, String statisticsDate, String userId, Integer dataType);
+
+
+    /**
+     * 根据StatisticsDateAndUserIdAndDataType删除
+     */
+    Integer deleteStatisticsInfoByStatisticsDateAndUserIdAndDataType(String statisticsDate, String userId, Integer dataType);
+
+    /**
+     * 统计数据
+     */
+    void statisticsData();
+
+    Map<String, Integer> getStatisticsInfoActualTime(String userId);
+
+    /**
+     * 查询总数
+     *
+     * @param param
+     * @return
+     */
+    List<StatisticsInfo> findListTotalInfoByParam(StatisticsInfoQuery param);
+
+    List<StatisticsInfo> findUserCountTotalInfoByParam(StatisticsInfoQuery param);
+
+}
